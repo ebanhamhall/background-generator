21011c274ea2187dc27d4bc937057e08822e0ecf
ebanhamhall <e.banhamhall@gmail.com>
2020-05-11 15:25:36 +0100

    Starting files

---
+++ main.js
@@ -0,0 +1,41 @@
+document.querySelector("h1").style.color = prompt("What is your favourite colour?");
+
+var user = {
+	name: "Ed",
+	age: 40,
+	hobby: "JavaScript",
+	isMarried: true,
+};
+
+var wizard = {
+	spells: ["Fly", "blow up", "Card trick"],
+	name: "Fred",
+	hairColour: "grey",
+	shout: function(){
+		console.log("Boom!");
+	}
+};
+
+var facebookUser = {
+	username: "Edward",
+	password: "password123",
+};
+
+var database = [facebookUser];
+
+var person1 = {
+	username: "person1",
+	timeline: "timeline1",
+}
+
+var person2 = {
+	username: "person2",
+	timeline: "timeline2",
+}
+
+var person3 = {
+	username: "person3",
+	timeline: "timeline3",
+}
+
+var newsfeed = [person1, person2, person3];
